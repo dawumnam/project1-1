1d4ddbc14d0a23ad79eef5b4da8e721506ce27b4
Dawum Nam <dawumnam@gmail.com>
2021-04-28 17:22:56 -0800

    Initial Commit

---
+++ project1-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project1-1
+//
+//  Created by dawum nam on 4/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
